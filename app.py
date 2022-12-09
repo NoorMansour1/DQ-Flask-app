3a5ffba635a5581bc57e2a98cbaf98c443b63778
Noor <noormansour2303@gmail.com>
2022-12-09 09:34:17 +0100

    update app.py

--- app.py
+++ app.py
@@ -87,44 +87,20 @@ def processing():
 #     return {"example": ex}
 
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-    # now = datetime.now()
-    # formatted_now = now.strftime("%A, %d, %B, %Y, at %X")
+# @app.route("/report/min/html")
+# def get_min_html_report():
+#     return app.send_static_file("report/report_min.html")
 
-    # match_object = re.match("[a-zA-Z]+", name)
-
-    # if match_object: 
-    #     clean_name = match_object.group(0)
-    # else:
-    #     clean_name = "Friend"
-
-    # content = "Hello there, " + clean_name + "! It's " + formatted_now
-
-    # return content
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
-
-@app.route("/report/min/html")
-def get_min_html_report():
-    return app.send_static_file("report/report_min.html")
-
+# Return the static html report file, possible for different report types (min or max) 
 @app.route("/report/max/html")
 def get_max_html_report():
     return app.send_static_file("report/report_max.html")
 
-@app.route("/report/min/json")
-def get_min_json_report():
-    return app.send_static_file("report/report_min.json")
+# @app.route("/report/min/json")
+# def get_min_json_report():
+#     return app.send_static_file("report/report_min.json")
 
+# Return the static json report file, possible for different report types (min or max) 
 @app.route("/report/max/json")
 def get_max_json_report():
     return app.send_static_file("report/report_max.json")
